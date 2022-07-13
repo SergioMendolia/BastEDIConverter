c0a85f40e23f51585b6b6eb6f2cbba3ba9356401
Sergio Mendolia <sergio.mendolia@liip.ch>
2022-07-13 10:55:10 +0200

    fixes for alma

--- src/Controller/EdiConverterController.php
+++ src/Controller/EdiConverterController.php
@@ -92,6 +92,7 @@ class EdiConverterController extends AbstractController
                         ->setSpecificationText($facture['TITRE'][$key] . ' ' . $facture['AUTEUR'][$key] . ' (' . $facture['PRIX_UNITAIRE'][$key] . ')')
                         ->setAdditionalText('TVA' . $facture['TVA_TAUX'][$key])
                         ->setGrossPrice($this->cleanupPriceDegueu($facture['PRIX_BRUT'][$key]))
+                        ->setNetPrice($this->cleanupPriceDegueu($facture['PRIX_NET'][$key]))
                         ->setMOANetPrice($this->cleanupPriceDegueu($facture['PRIX_NET'][$key]));
                     if ($facture['VREF'][$key] !== '') {
                         $item->setDeliveryNoteNumber('' . $facture['VREF'][$key]);
